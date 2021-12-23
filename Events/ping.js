25518438fda499dd285e6994b95d79531642c293
khanhmuy <maikhanhhuy0608@outlook.com>
2021-12-23 18:10:56 +0700

    i hate naming commits

--- Events/ping.js
+++ Events/ping.js
@@ -8,10 +8,14 @@ module.exports = {
 		const prefix = client.data.get(`guild.${message.guild.id}.prefix`);
 		if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) {
 			const embed = new MessageEmbed()
-				.setTitle('Hi! I\'m Eider!')
-				.setDescription(`My prefix in this guild is currently \`${prefix}\``)
+				.setTitle('Hi! I\'m **Eider**. A fully free and open source Discord bot.')
+				.setDescription('I\'m a Discord Bot made by CallMeEcho#0253 and modified by hmuy#4638, built with [Node.js](https://nodejs.org) and [Discord.js](https://discord.js.org).\n')
+				.setThumbnail('https://cdn.discordapp.com/attachments/858855894204678206/874231112686247956/eider-animate.gif')
 				.setColor('BLURPLE')
-				.setThumbnail('https://cdn.discordapp.com/attachments/858855894204678206/874231112686247956/eider-animate.gif');
+				.addFields(
+					{ name: 'To get started:', value: 'Use `' + prefix + 'help` to see all my commands.' },
+					{ name: 'Invite link:', value: 'https://tiny.cc/eider_hmuy'},
+				)
 			message.channel.send({ embeds: [ embed ] });
 		}
 	},
