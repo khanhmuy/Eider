c99a784f5e2f85af93a979657d414856ef5f6233
khanhmuy <maikhanhhuy0608@outlook.com>
2022-01-01 14:57:23 +0700

    update Events/start.js

--- Events/start.js
+++ Events/start.js
@@ -5,8 +5,8 @@ module.exports = {
 		client.user.setStatus({
 			status: 'online',
 		});
-		client.user.setActivity('Just got started up!');
-		console.log(`Connection established (${client.ws.ping}ms). Connected as ${client.user.username}#${client.user.discriminator} (${client.user.id})`)
+		client.user.setActivity('`,help`');
+		console.log(`Connection established (${client.ws.ping}ms). Logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`)
 		setInterval(() => {
 			let now = require('./../status.json')[Math.floor(Math.random() * require('./../status.json').length)]
 			if (!now.status) now.status = 'dnd';
@@ -16,9 +16,6 @@ module.exports = {
 			  })
 			  client.user.setActivity(now.name)
 			}
-			console.log(now.name)
-			console.log(now.type)
-			console.log(now.status)
 		}, 15000)
 	},
 };
