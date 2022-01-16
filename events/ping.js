aeca60e168bd28bd1bc35c662a425099a72329d8
khanhmuy <maikhanhhuy0608@outlook.com>
2022-01-16 10:28:18 +0700

    current prefix

--- events/ping.js
+++ events/ping.js
@@ -9,7 +9,7 @@ module.exports = {
 		if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) {
 			const embed = new MessageEmbed()
 				.setTitle('Hi! I\'m **Eider**. A fully free and open source Discord bot.')
-				.setDescription('I\'m a Discord Bot made by CallMeEcho#0253 and modified by [hmuy#4638](https://discord.com/users/272388882539085824), built with [Node.js](https://nodejs.org) and [Discord.js](https://discord.js.org).\n')
+				.setDescription('I\'m a Discord Bot made by CallMeEcho#0253 and modified by [hmuy#4638](https://discord.com/users/272388882539085824), built with [Node.js](https://nodejs.org) and [Discord.js](https://discord.js.org).\nMy prefix is currently `' + prefix + '`')
 				.setThumbnail('https://cdn.discordapp.com/attachments/858855894204678206/874231112686247956/eider-animate.gif')
 				.setColor('#C0DDFF')
 				.addFields(
