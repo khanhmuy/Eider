9ba0b24a332cfc37933b5172deb9f38911f0868a
khanhmuy <maikhanhhuy0608@outlook.com>
2022-01-17 10:55:35 +0700

    thank you!

--- commands/Miscellaneous/credit.js
+++ commands/Miscellaneous/credit.js
@@ -17,7 +17,8 @@ module.exports = {
                 { name: 'CallMeEcho', value: 'The creator of this bot, who also helped me a lot in the early stages of learning Discord bot development.\n\n[Echo\'s GitHub](https://github.com/CallMeEchoCodes)' },
                 { name: 'llsc12', value: 'For helping me when I have issues with development in general, the developer of [llsc12.ml](https://llsc12.ml) used in the `videoinfo` command.\n\n[llsc12\'s GitHub](https://github.com/llsc12)' },
                 { name: 'API\'s used in this bot', value: '[PronounDB](https://pronoundb.org), [NASA\'s OpenAPIs](https://api.nasa.gov/), [Inspiration API](https://inspiration.goprogram.ai/), [Urban Dictionary API](https://www.urbandictionary.com/), [Useless Facts API](https://uselessfacts.jsph.pl/random.json?language=en), [Bored API](https://www.boredapi.com/api/activity/), [Return Youtube Dislikes API](https://returnyoutubedislike.com).'},
-                { name: 'Eider is also somewhat based off of', value: '[Aquacious](https://github.com/Aquacious/Aquacious) :trol:' }
+                { name: 'Eider is also somewhat based off of', value: '[Aquacious](https://github.com/Aquacious/Aquacious) :trol:' },
+                { name: 'And a big, big thanks to SillySock for letting me be hosted on their VPS', value: '\n[SillySock\'s GitHub](https://github.com/Sillysockk)' }
             )
         message.channel.send({ embeds: [embed] });
     }
--- events/ping.js
+++ events/ping.js
@@ -7,16 +7,25 @@ module.exports = {
 
 		const prefix = client.data.get(`guild.${message.guild.id}.prefix`);
 		if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) {
-			const embed = new MessageEmbed()
+			const about = new MessageEmbed()
 				.setTitle('Hi! I\'m **Eider**. A fully free and open source Discord bot.')
 				.setDescription('I\'m a Discord Bot made by CallMeEcho#0253 and modified by [hmuy#4638](https://discord.com/users/272388882539085824), built with [Node.js](https://nodejs.org) and [Discord.js](https://discord.js.org).\nMy prefix is currently `' + prefix + '`')
 				.setThumbnail('https://cdn.discordapp.com/attachments/858855894204678206/874231112686247956/eider-animate.gif')
 				.setColor('#C0DDFF')
-				.addFields(
-					{ name: 'To get started:', value: 'Use `' + prefix + 'help` to see all my commands.' },
-					{ name: 'Links', value: '[Invite](https://hmuy.ml/eider) | [Source code](https://github.com/khanhmuy/eider_hmuy)' }
-				)
-			message.channel.send({ embeds: [ embed ] });
+				.addField('To get started:', 'Use `' + prefix + 'help` to see all my commands.')
+				.setFooter('Thank you for adding Eider, we really appreciate it!');
+			const invite = new MessageEmbed()
+				.setTitle('Invite Eider to your servers!')
+				.setColor('#C0DDFF')
+				.setDescription('[Click here to invite Eider to your servers!](https://hmuy.ml/eider)')
+				.addField('You can also join our support server:', '[Click here to join our support server!](https://discord.gg/rKuH5TRDy9)')
+				.setURL('https://hmuy.ml/eider')
+			const github = new MessageEmbed()
+				.setTitle('Eider\'s Github repositories')
+				.setColor('#C0DDFF')
+				.setDescription('[Click here to view Eider_hmuy\'s Github repositiory!](https://github.com/khanhmuy/eider_hmuy)\n[Click here to view Eider\'s Github repositiory!](https://github.com/CallMeEchoCodes/Eider)')
+
+			message.channel.send({ embeds: [ about, invite, github ] });
 		}
 	},
 };
