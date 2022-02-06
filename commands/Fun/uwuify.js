4987ac5c53214efb234ef7148ebdeb9fea3f8f21
khanhmuy <maikhanhhuy0608@outlook.com>
2022-02-06 20:25:19 +0700

    uwuifier

---
+++ commands/Fun/uwuify.js
@@ -0,0 +1,29 @@
+const Uwuifier = require('uwuifier');
+const uwuifier = new Uwuifier();
+const {MessageEmbed} = require('discord.js'); 
+module.exports = {
+    name: 'uwuifier',
+    description: 'uwuifi some text',
+    usage: 'uwuifier [text]',
+    cooldown: 2,
+    aliases: ['uwu', 'uwuify'],
+    async execute(client, message, args) {
+        try {
+            const text = args.join(' ');
+            if (!text) return message.reply('You need to provide some text!');
+            else {
+                const uwuified = uwuifier.uwuifySentence(text);
+                const embed = new MessageEmbed()
+                    .setAuthor(message.author.username, `${message.author.displayAvatarURL({ dynamic: true })}?size=1024`)
+                    .setColor('BLUE')
+                    .setDescription(uwuified);
+                message.delete();
+                message.channel.send({ embeds: [embed] });
+            }
+        }
+        catch (error) {
+            console.error(error);
+            message.reply('There was an error trying to execute that command!');
+        }
+    }
+};
