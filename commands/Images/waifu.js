cb2196b017db4f3f592b15411a9a95157c0e12f2
khanhmuy <maikhanhhuy0608@outlook.com>
2022-02-22 20:43:48 +0700

    waifus

---
+++ commands/Images/waifu.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const Vibrant = require('node-vibrant');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+module.exports = {
+    name: 'waifu',
+    description: 'Sends a random (SFW) waifu image! (credit where credit is due: LillieWeeb001 on GitHub)',
+    usage: 'waifu',
+    cooldown: 3,
+    async execute (client, message) {
+        try {
+            const wait = await message.channel.send('Fetching...');
+            const res = await axios.get('https://api.waifu.pics/sfw/waifu')  
+            let color = null
+            color = await Vibrant.from(res.data.url).getPalette()
+            color = color.Vibrant.hex
+            const embed = new MessageEmbed()
+                .setTitle('Waifu image')
+                .setColor(color)
+                .setTimestamp()
+                .setImage(res.data.url)
+                .setURL(res.data.url)
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setStyle('LINK')
+                        .setURL(res.data.url)
+                        .setLabel('View Orginal Image')
+                )
+            wait.delete();
+            message.reply({ embeds: [embed], components: [row], allowedMentions: { repliedUser: false } });
+        }
+        catch (error) {
+            wait.delete();
+            console.log(error);
+            message.reply('Something went wrong, try again later.').then(x => {
+                setTimeout(() => {
+                    x.delete();
+                }, 4000)
+            });
+        }
+    },
+};
