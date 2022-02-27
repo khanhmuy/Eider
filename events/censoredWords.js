13671d880769b11ae000188c031449430f8880a6
khanhmuy <maikhanhhuy0608@outlook.com>
2022-02-27 08:19:42 +0700

    copypastas bot exists now

--- events/censoredWords.js
+++ events/censoredWords.js
@@ -17,17 +17,6 @@ module.exports = {
                     
                 }
             });
-            //words.data.scamdiscordurls.forEach(word => {
-                //if (message.content.includes(word)) {
-                    //message.delete();
-                    //const mention = '<@!' + message.author.id + '>';
-                    //const embed = new MessageEmbed()
-                        //.setTitle('Fake or scam discord detected!')
-                        //.setDescription('Your message contained the link to a Discord scam link (' + word + '). Your message has been removed.')
-                        //.setColor('#ff0000')
-                    //message.channel.send( {content: mention, embeds: [embed]} )
-                //}
-            //});
             words.data.scamideviceunlockurls.forEach(word => {
                 if (message.content.includes(word)) {
                     message.delete();
@@ -39,20 +28,6 @@ module.exports = {
                     message.channel.send( {content: mention, embeds: [embed]} )
                 }
             });
-            const whoasked = {
-                whoasked: [
-                    "ai hỏi",
-                    "who asked",
-                    "who fucking asked",
-                    "whoasked",
-                    "whofuckingasked"
-                ]
-            };
-            whoasked.whoasked.forEach(word => {
-                if (message.content.includes(word)) {
-                    message.reply({content: 'Ai mượn bạn nói mà cái địt mẹ bạn lúc lồn nào cũng "aI hỎi?". Là tôi, tôi đã hỏi. Thật là một cực hình khi phải nặng gánh trách nhiệm của một kẻ tội đồ đã đưa ra câu hỏi và giấu mình vào ngôi thứ ba. Trách nhiệm nặng như quả đất, đặt vào đôi bờ vai nhỏ bé của tôi. Nó như đè nát thân tâm tôi, nó rằng xé con người tôi bởi sức nặng khủng khiếp của Kẻ Đã Hỏi. Thật là một cực hình, quả là địa ngục trần gian, đúng là tra tấn man rợn. Nhưng cuối cùng, tôi cũng được giải phóng khỏi chuỗi ngày đau khổ đó. Đúng, là tôi, tôi đã hỏi, có vấn đề gì không?', allowedMentions: { repliedUser: false }})
-                }
-            })
         } catch(error) {
             console.log(error);
         }
