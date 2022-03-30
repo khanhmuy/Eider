43c42dc4bd0c827a474f325188281f3f41fd4b73
khanhmuy <maikhanhhuy0608@outlook.com>
2022-03-30 11:22:25 +0700

    more logging

---
+++ events/logging/unbannedMember.js
@@ -0,0 +1,19 @@
+const {MessageEmbed} = require('discord.js');
+module.exports = {
+    name: 'guildBanRemove',
+    async execute(client, member) {
+        try {
+            const embed = new MessageEmbed()
+                .setAuthor(member.user.username + '#' + member.user.discriminator, `${member.user.displayAvatarURL({ dynamic: true })}?size=1024`)
+                .setDescription(`<@!${member.user.id}> has been unbanned from the server.`)
+                .setThumbnail(`${member.user.displayAvatarURL({ dynamic: true })}?size=1024`)
+                .setColor('RED')
+                .setFooter(`${member.guild.name}`)
+                .setTimestamp();
+            const logChannel = client.channels.cache.get(client.data.get(`guild.${member.guild.id}.logChannel`));
+            logChannel.send({embeds: [embed]});
+        } catch (err) {
+            console.log(err);
+        }
+    }
+};
