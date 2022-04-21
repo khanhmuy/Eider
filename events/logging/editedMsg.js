f68d6a81b4956030817791eaaa2faf9c7593936a
khanhmuy <maikhanhhuy0608@outlook.com>
2022-04-21 18:16:03 +0700

    :moyai:

--- events/logging/editedMsg.js
+++ events/logging/editedMsg.js
@@ -3,7 +3,8 @@ module.exports = {
     name: 'messageUpdate',
     async execute (client, message) {
         try {
-            if (message.embeds.length == 0) {
+            console.log(message);
+            if (message.reactions.embeds.length == 0) {
                 let deleteEmbed = new MessageEmbed()
                     .setAuthor(message.author.username + '#' + message.author.discriminator, `${message.author.displayAvatarURL({ dynamic: true })}?size=1024`)
                     .setDescription(`:pencil2: Message edited in <#${message.channelId}>. [Jump to message](https://discordapp.com/channels/${message.guild.id}/${message.channelId}/${message.id})`)
