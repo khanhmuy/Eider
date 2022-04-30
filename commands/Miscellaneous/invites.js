4b1aa696e22718bcdc9e4ca43dcb0b0a929c06e4
khanhmuy <maikhanhhuy0608@outlook.com>
2022-04-30 09:19:07 +0700

    server invites command

---
+++ commands/Miscellaneous/invites.js
@@ -0,0 +1,36 @@
+const {Permissions, MessageEmbed} = require('discord.js');
+const moment = require('moment');
+module.exports = {
+    name: 'serverinvites',
+    description: 'Get the server\'s invite links',
+    usage: 'serverinvites',
+    aliases: ['invites', 'invitations', 'serverinvite', 'serverinvitations'],
+    cooldown: 5,
+    guildOnly: true,
+    permissions: [ Permissions.FLAGS.CREATE_INSTANT_INVITE ],
+    async execute(client, message, args) {
+        try {
+            const guild = await message.guild.fetch();
+            const invites = await message.guild.invites.fetch();
+            console.log(invites);
+            const embed = new MessageEmbed()
+                .setTitle(`Invites for ${message.guild.name}`)
+                .setThumbnail(guild.iconURL())
+                .setColor('BLUE')
+                .setTimestamp()
+                .setFooter('Last Updated')
+            invites.forEach(invite => {
+                embed.addField(`${invite.code}`, `Uses: ${invite.uses}\nMax uses: ${invite.maxUses}\nCreated at: ${moment(invite.createdTimestamp).format('LLLL')}\nCreated by: <@!${invite.inviter.id}>\nChannel: <#${invite.channel.id}>`)
+            });
+            message.reply({embeds: [embed], allowedMentions: { repliedUser: false }});
+        } catch(err) {
+            console.log(err);
+            message.reply('An unknown error happened').then(x => {
+                setTimeout(() => {
+                    message.delete();
+                    x.delete();
+                }, 4000)
+            })
+        }
+    }
+};
